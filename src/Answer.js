f2d43ebb2eb2d8e740a23f01cf2112e0c4091a20
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-10-04 22:55:47 +0200

    homescreen styling done

--- src/Answer.js
+++ src/Answer.js
@@ -1,12 +1,19 @@
-function Answer({answer, isCorrectAnswer, onHandleAnswerChange, isChecked}){
-
-    return (
-        <div >
-            <input type="radio" name="answer" id={answer} value={answer} onChange={onHandleAnswerChange} />
-            <label dangerouslySetInnerHTML={{__html:answer}} htmlFor={answer}></label>
-            
-        </div>
-    )
+function Answer({ answer, isCorrectAnswer, onHandleAnswerChange, isChecked }) {
+  return (
+    <div>
+      <input
+        type="radio"
+        name="answer"
+        id={answer}
+        value={answer}
+        onChange={onHandleAnswerChange}
+      />
+      <label
+        dangerouslySetInnerHTML={{ __html: answer }}
+        htmlFor={answer}></label>
+    </div>
+  );
 }
 
-export default Answer
+export default Answer;
+ 
