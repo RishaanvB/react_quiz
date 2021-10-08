543387c03d6cd5a9634a9979b93672658fcdc496
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-10-08 20:57:03 +0200

    finish questionview

--- src/components/QuestionProgress.js
+++ src/components/QuestionProgress.js
@@ -1,14 +1,17 @@
-export default function QuestionProgress({currentQuestion}) {
+export default function QuestionProgress({ currentQuestion }) {
   return (
     <div className="progression-container">
+      <div className="progression-bar">
+        <span
+          style={{ width: currentQuestion * 10 + "%" }}
+          className="progress-bar-fill"></span>
+      </div>
       <span className="questions-progression-container">
         <span className="questions-percentage">
-          {currentQuestion} <span className="question-percentage-divider">/</span> 10
+          {currentQuestion}{" "}
+          <span className="question-percentage-divider">/</span> 10
         </span>
       </span>
-      <div className="progression-bar">
-        <span style={{width: currentQuestion*10 + '%'}} className="progress-bar-fill"></span>
-      </div>
     </div>
   );
 }
