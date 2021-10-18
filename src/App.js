eff99775be5b603881112e3f3f422e7500551669
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-10-18 16:53:59 +0200

    update rename viewfolder

--- src/App.js
+++ src/App.js
@@ -1,69 +1,68 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
-import QuestionView from "./views/QuestionView";
-import ResultView from "./views/ResultView";
-import HighscoreView from "./views/HighscoreView";
-import HomeScreen from "./views/HomeScreen";
+import QuestionView from './views/QuestionView';
+import ResultView from './views/ResultView';
+import HighscoreView from './views/HighscoreView';
+import HomeScreen from './views/HomeScreen';
 
-import { animateButton } from "./helpers/helpers";
+import { animateButton } from './helpers/helpers';
 
 function App() {
-  const [gameState, setGameState] = useState("result");
+  const [gameState, setGameState] = useState('home');
 
   const [score, setScore] = useState(0);
   // const [maxRounds, setMaxRounds] = useState(5);
-  const maxRounds = 5
-  
-  
-  console.log("score is ==>", score);
+  const maxRounds = 5;
+
+  console.log('score is ==>', score);
   const updateScore = (point = 1) => {
     setScore(score + point);
   };
 
   const showHighscoreView = (e) => {
-    console.log("display highscore view");
-    setGameState("highscores");
+    console.log('display highscore view');
+    setGameState('highscores');
     animateButton(e.target);
   };
 
   const showQuestionView = () => {
-    console.log("start quiz");
-    setGameState("quiz");
+    console.log('start quiz');
+    setGameState('quiz');
   };
   const goHome = () => {
-    setGameState("home");
+    setGameState('home');
   };
   const showResultScreen = () => {
-    console.log("score is ==>", score);
-    setGameState("result");
+    console.log('score is ==>', score);
+    setGameState('result');
   };
 
   const displayScreen = () => {
     switch (gameState) {
-      case "home":
+      case 'home':
         return (
           <HomeScreen
             handleHighscoreView={showHighscoreView}
             handleQuestionView={showQuestionView}
           />
         );
-      case "quiz":
+      case 'quiz':
         return (
           <>
             <h1>{score}</h1>
             <QuestionView
-            maxRounds={maxRounds}
+              maxRounds={maxRounds}
               score={score}
               showResultScreen={showResultScreen}
               updateScore={updateScore}
             />
           </>
         );
-      case "result":
-        return <ResultView score={score} maxRounds={maxRounds}  />;
-      case "highscores":
-        return <HighscoreView  />;
+      case 'result':
+        return <ResultView score={score} maxRounds={maxRounds} />;
+      case 'highscores':
+        return <HighscoreView />;
 
       default:
         break;
@@ -75,7 +74,7 @@ function App() {
       <div className="container">
         {displayScreen()}
 
-        <button style={{ position: "absolute" }} onClick={goHome}>
+        <button style={{ position: 'absolute' }} onClick={goHome}>
           Home
         </button>
       </div>
