e5e6e5aa2ffd82f333d9661ad7ef1c7a7f4c2d1e
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-10-28 22:19:37 +0200

    code: add highscore and restart btn on highscore submit

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -5,15 +5,15 @@ import FormError from './FormError';
 export default function Form({ score }) {
   const [isValid, setIsValid] = useState(false);
   const [input, setInput] = useState('');
+  const [scoreSubmitted, setScoreSubmitted] = useState(false);
   const inputEl = useRef(null);
-  
+
   useEffect(() => validate());
-  
+
   useEffect(() => {
     inputEl.current.focus();
   }, []);
-  
-  
+
   const validate = () => {
     input.match(/^[a-zA-Z0-9_]{1,8}$/) && setIsValid(true);
     !input.match(/^[a-zA-Z0-9_]{1,8}$/) && setIsValid(false);
@@ -27,9 +27,34 @@ export default function Form({ score }) {
   const handleSubmitScore = (e) => {
     e.preventDefault();
     const username = e.target.username.value;
+    setScoreSubmitted(true);
     setHighscore({ username: username, score: score });
   };
+
   const showFormError = () => (input.length > 0 && !isValid ? true : false);
+
+  const submitBtnEl = (
+    <button
+      disabled={!isValid}
+      className={
+        !isValid ? 'btn-submit-score btn btn-disabled' : 'btn-submit-score btn'
+      }>
+      Submit Score
+    </button>
+  );
+
+  const restartGameBtnEl = <button key='restart'>Restart</button>;
+  const displayHighscoreBtnEl = <button key='highscore'>Highscores</button>;
+  const textInputEl = (<input
+    className="btn"
+    type="text"
+    name="username"
+    id="username"
+    placeholder="Your Name"
+    value={input}
+    onChange={handleChange}
+    ref={inputEl}
+  />)
 
   return (
     <form
@@ -37,27 +62,9 @@ export default function Form({ score }) {
       onSubmit={handleSubmitScore}
       action=""
       method="POST">
-      <input
-        className="btn"
-        type="text"
-        name="username"
-        id="username"
-        placeholder="Your Name"
-        value={input}
-        onChange={handleChange}
-        ref={inputEl}
-      />
+      {!scoreSubmitted && textInputEl}
       {showFormError() && <FormError inputLength={input.length} />}
-
-      <button
-        disabled={!isValid}
-        className={
-          !isValid
-            ? 'btn-submit-score btn btn-disabled'
-            : 'btn-submit-score btn'
-        }>
-        Submit Score
-      </button>
+      {scoreSubmitted ? [restartGameBtnEl, displayHighscoreBtnEl] : submitBtnEl}
     </form>
   );
 }
